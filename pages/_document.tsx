9754ec8b814c9c019c2bbcf9104c79d3302714be
kjin <pkjin.eng@gmail.com>
2024-02-06 23:55:42 +0900

    feat: change index

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -6,6 +7,11 @@ export default function Document() {
       <Head />
       <body>
         <Main />
+        <Script
+          type="text/javascript"
+          strategy="beforeInteractive"
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_MAP_CLIENT_ID}`}
+        />
         <NextScript />
       </body>
     </Html>
