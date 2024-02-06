102f8ca60a6fb83f17c3574bd5f66a45da4d94a4
kjin <pkjin.eng@gmail.com>
2024-02-06 23:53:08 +0900

    feat: update api

---
+++ pages/api/data.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+let list: { date: string; time: string; name: string } = {
+  date: "",
+  time: "",
+  name: "",
+};
+
+const handler = async (req: any, res: any) => {
+  if (req.method === "GET") {
+    const getAPI = async (req: Request) => {
+      const API_URL = `http://openAPI.seoul.go.kr:8088/${
+        process.env.NEXT_PUBLIC_API_URL_KEY
+      }/json/TimeAverageAirQuality/1/25/${list.date + list.time}/${list.name}`;
+      let response;
+      try {
+        response = await axios.get(API_URL);
+      } catch (error) {
+        console.log(error);
+      }
+      console.log(response);
+      return response;
+    };
+    await getAPI(req).then((response) => {
+      res.json(response?.data);
+    });
+  } else if (req.method === "POST") {
+    list = {
+      date: req.body.date,
+      time: req.body.time,
+      name: req.body.name,
+    };
+    res.send(list);
+  }
+};
+
+export default handler;
