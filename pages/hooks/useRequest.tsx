66b8a0ebc3047578f7ee03af0a2bfd569387de01
kjin <pkjin.eng@gmail.com>
2024-02-06 23:55:25 +0900

    feat: add hooks

---
+++ pages/hooks/useRequest.tsx
@@ -0,0 +1,53 @@
+import { useDispatch } from "react-redux";
+import { todayData, yesterdayData, totalData } from "../store/dataSlice";
+
+export default function useGetData() {
+  const dispatch = useDispatch();
+
+  const getData = async (dateType: string) => {
+    try {
+      const response = await fetch("/api/data");
+      const result = await response.json();
+      if (!result.RESULT) {
+        const data = result.TimeAverageAirQuality.row;
+        if (dateType === "today") {
+          dispatch(todayData(data));
+        } else if (dateType === "yesterday") {
+          dispatch(yesterdayData(data));
+        } else if (dateType === "total") {
+          dispatch(totalData(data));
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const updateData = async (
+    reqDate: string,
+    reqTime: string,
+    reqName: string | undefined
+  ) => {
+    try {
+      const response = await fetch("/api/data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          date: reqDate,
+          time: reqTime,
+          name: reqName,
+        }),
+      });
+      if (!response.ok) {
+        console.log("error");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return { getData, updateData };
+}
